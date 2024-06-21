689356a5367e86417cfeb6b9b99639c67c82afed
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-21 19:44:33 +0200

    cambiar posición span

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,101 +20,101 @@ const Navbar = () => {
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/tracker1"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-pie-chart hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-pie-chart group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/tracker2"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-view-list hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-view-list group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/tracker3"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-view-grid hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-view-grid group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/tracker4"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-layout-tab hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-layout-tab group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-layers-alt hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-layers-alt group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-stats-up hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-stats-up group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-world hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-world group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
 
-          <li className=" flex-auto mx-1 my-5 p-0 text-center">
+          <li className="group flex-auto mx-1 my-5 p-0 text-center">
             <NavLink
               to="/"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-comment hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-comment group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
-          <li className="mx-1 my-5 p-0 text-center">
+          <li className="group mx-1 my-5 p-0 text-center">
             <NavLink
               to="/"
               className="hover:bg-primary rounded-lg text-center px-4 py-3 outline-none relative"
               data-tab="chats"
             >
-              <span className="block w-2 h-2 rounded-lg absolute top-3 right-3 bg-warning"></span>
-              <i className="ti-flickr-alt hover:text-white text-primary text-2xl relative top-1"></i>
+              <span className="block w-2 h-2 rounded-lg absolute top-1 right-1 bg-warning"></span>
+              <i className="ti-flickr-alt group-hover:text-white text-primary text-2xl relative top-1"></i>
             </NavLink>
           </li>
         </ul>
