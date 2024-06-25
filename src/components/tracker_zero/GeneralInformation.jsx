b3bf4c90aca03bbfea07c00bdf5dbe9ab302dd14
Deiximar Virriel <deiximar@gmail.com>
2024-06-25 11:43:14 +0200

    Compontenizar la seccion de informacion general de tracker 0

--- src/components/tracker_zero/GeneralInformation.jsx
+++ src/components/tracker_zero/GeneralInformation.jsx
@@ -1,5 +1,6 @@
 import useApi from "../../services/useApi";
 import { API_BASE_URL } from "../../config/urls.js";
+import InformationCard from "./InformationCard.jsx";
 
 const GeneralInformation = () => {
   const data = useApi(`${API_BASE_URL}/all`);
@@ -14,47 +15,36 @@ const GeneralInformation = () => {
         COVID-19 Tracker
       </h2>
       <ul className="list-none">
-        <li className="bg-lightRed p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent hover:border-brightRed hover:scale-105 cursor-pointer">
-          <h6 className="text-base font-semibold text-navyBlue inline-block">
-            Total Case
-          </h6>
-          <p className="text-2xl text-brightRed font-semibold leading-6 float-right">
-            {data.cases}
-          </p>
-          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
-            +{data.todayCases}
-          </b>
-        </li>
-        <li className="bg-lightOrange p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent hover:border-brightOrange hover:scale-105 cursor-pointer">
-          <h6 className="text-base font-semibold text-navyBlue inline-block">
-            Active Case
-          </h6>
-          <p className="text-2xl text-brightOrange font-semibold leading-6 float-right">
-            {data.active}
-          </p>
-        </li>
-        <li className="bg-lightGreen p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent hover:border-brightGreen hover:scale-105 cursor-pointer">
-          <h6 className="text-base font-semibold text-navyBlue inline-block">
-            Recovered Case
-          </h6>
-          <p className="text-2xl text-brightGreen font-semibold leading-6 float-right">
-            {data.recovered}
-          </p>
-          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
-            +{data.todayRecovered}
-          </b>
-        </li>
-        <li className="bg-lightBlue p-5 pb-4 mb-4 rounded border-2 border-solid border-transparent hover:border-primary hover:scale-105 cursor-pointer">
-          <h6 className="text-base font-semibold text-navyBlue inline-block">
-            Deaths Case
-          </h6>
-          <p className="text-2xl text-primary font-semibold leading-6 float-right">
-            {data.deaths}
-          </p>
-          <b className="float-right text-xs text-steel bg-white p-1 mr-2 rounded tracking-wide">
-            +{data.todayDeaths}
-          </b>
-        </li>
+        <InformationCard
+          title={"Total Case"}
+          cases={data.cases}
+          todayCases={data.todayCases}
+          textColor="brightRed"
+          bgColor="lightRed"
+        />
+
+        <InformationCard
+          title={"Active Case"}
+          cases={data.active}
+          textColor="brightOrange"
+          bgColor="lightOrange"
+        />
+
+        <InformationCard
+          title={"Recovered Case"}
+          cases={data.recovered}
+          todayCases={data.todayRecovered}
+          textColor="brightGreen"
+          bgColor="lightGreen"
+        />
+
+        <InformationCard
+          title={"Deaths Case"}
+          cases={data.deaths}
+          todayCases={data.todayDeaths}
+          textColor="primary"
+          bgColor="lightBlue"
+        />
       </ul>
     </>
   );
