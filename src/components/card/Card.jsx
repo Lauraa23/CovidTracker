43e295ca7ec6f2f7a00e266e614a999183d1e458
helopgom <helena.lopez.gomez@gmail.com>
2024-06-25 14:21:42 +0200

    Cambios en Tailwind

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ children }) => {
     <div id="container" className="max-w-2/4 w-6/12 px-4">
       <div
         id="card"
-        className="group relative overflow-hidden rounded shadow-[0_15px_30px_0_rgba(20,50,90,0.05)] p-[15px] hover:translate-y-[-10px]"
+        className=" bg-white group relative overflow-hidden rounded shadow-[0_15px_30px_0_rgba(20,50,90,0.05)] p-[15px] py-4 hover:translate-y-[-10px]"
       >
         <div id="info">{children}</div>
       </div>
@@ -18,7 +18,7 @@ export default Card;
 
 export const CardTitle = ({ children }) => {
   return (
-    <h5 className="text-lg font-medium text-[black] mt-[-15px] mb-2.5 -mx-5 px-5 py-[15px] border-b-[#3639ae] border-b border-solid">
+    <h5 className="text-lg font-medium text-black mt-[-15px] mb-2.5 -mx-5 px-5 py-[15px] border-b border-strongGray">
       {children}
     </h5>
   );
--- src/components/tracker_one/TrackerOne.jsx
+++ src/components/tracker_one/TrackerOne.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import Card, { CardTitle, CardData, CardImage } from "../card/Card";
 import { TotalCases } from "./TotalCases";
 import { TotalDeaths } from "./TotalDeaths";
-import "./trackerOne.css";
+// import "./trackerOne.css";
 import { NewCases } from "./NewCases";
 import { NewDeaths } from "./NewDeaths";
 import { TotalRecovered } from "./TotalRecovered";
 import { TotalActive } from "./TotalActive";
-
+import Map from "../map/Map";
 const TrackerOne = () => {
   const [data, setData] = useState({
     totalCases: 31415926,
@@ -17,28 +17,44 @@ const TrackerOne = () => {
   return (
     <div
       id="rigthContent"
-      className="overflow-hidden block absolute w-3/5 bg-[#f7f8fc] left-[500px];
-    outline: none;"
+      className="overflow-hidden block absolute w-3/5 bg-[#f7f8fc] left-[500px]
+    outline-none"
     >
       <div
         id="containerSelector"
-        className="items-center justify-between flex mb-[30px] pb-[15px] border-b-[rgba(0,0,0,0.1)] border-b border-solid;"
+        className="items-center justify-between flex mb-[30px] pb-[15px] border-b border-solid border-[rgba(0,0,0,0.1)]"
       >
-        <div className="countrySelectBox">
+        <div
+          className="countrySelectBox flex items-center text-left relative select-none whitespace-nowrap"
+          style={{
+            fontSize: "16px",
+            borderRadius: "0 !important",
+            height: "50px",
+            padding: "8px 25px",
+            color: "#000",
+            fontWeight: 500,
+            width: "300px",
+            boxShadow: "0 15px 30px 0 rgba(20, 50, 90, 0.05)",
+            border: 0,
+            borderRadius: "4px !important",
+            lineHeight: "40px",
+            outline: "none",
+          }}
+        >
           <select>
             <option value="AL">Albania</option>
             <option value="AL">Albania</option>
           </select>
           <div className="countryPicker"></div>
         </div>
-        <div id="updapteOn" className="text-black font-medium text-base;">
+        <div id="updapteOn" className="text-black font-medium text-base">
           Updated: June 5, 2022
         </div>
       </div>
-      <div id="row" className="flex flex-wrap mr-[-15px] ml-[-15px];">
+      <div id="row" className="flex flex-wrap mr-[-15px] ml-[-15px]">
         <div
           id="rightInfo"
-          className="flex flex-wrap mr-[-15px] ml-[-15px] px-[15px];"
+          className="flex flex-wrap mr-[-15px] ml-[-15px] px-[15px]"
         >
           <TotalCases>{data.totalCases}</TotalCases>
           <TotalDeaths>{data.totalDeaths}</TotalDeaths>
@@ -47,7 +63,9 @@ const TrackerOne = () => {
           <NewCases>{data.newCases}</NewCases>
           <NewDeaths>{data.NewDeaths}</NewDeaths>
         </div>
-        <div className="Mapa"></div>
+        <div className="Mapa">
+          <Map />
+        </div>
       </div>
     </div>
   );
