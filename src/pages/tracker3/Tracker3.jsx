ba948ac610e3f55a9020005dc72228ba1512fab6
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-27 10:49:43 +0200

    instalar librería

--- src/pages/tracker3/Tracker3.jsx
+++ src/pages/tracker3/Tracker3.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+// import { ProductService } from "./serviceProductService";
 
 const Tracker3 = () => {
+  // const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   ProductService.getProductsMini().then((data) => setProducts(data));
+  // }, []);
+
   return (
     <div>
-      
+      <DataTable
+        rows={10}
+        // value={products}
+        showGridlines
+        tableStyle={{ minWidth: "50rem" }}
+      >
+        <Column field="Top Cases" header="Cases"></Column>
+        <Column field="Today Cases" header="Today Cases"></Column>
+        <Column field="Top Deaths" header="Top Deaths"></Column>
+        <Column field="Today Deaths" header="Today Deaths"></Column>
+        <Column field="Top Active" header="Top Active"></Column>
+        <Column field="Top Recover" header="Top Recover"></Column>
+      </DataTable>
     </div>
-  )
-}
+  );
+};
 
-export default Tracker3
+export default Tracker3;
