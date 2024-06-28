69159c1f988b0a217168e31fda8fbc362c859246
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-28 16:57:46 +0200

    estilo headers

--- src/pages/tracker3/Tracker3.jsx
+++ src/pages/tracker3/Tracker3.jsx
@@ -24,7 +24,7 @@ const Tracker3 = () => {
   }
   return (
     <div>
-      <div className="font-poppins text-[#2b2a69] text-2xl font-bold text-left mb-[30px] pb-[10px] pl-[20px] mt-[30px]">
+      <div className="font-poppins text-[#2b2a69] text-2xl font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px]">
         <h1>Top 10 Country wise Covid-19 Updates - Tiles</h1>
       </div>
 
@@ -34,19 +34,19 @@ const Tracker3 = () => {
             <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Top Cases</h4>
             </th>
-            <th>
+            <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Today Cases</h4>
             </th>
-            <th>
+            <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Top Deaths</h4>
             </th>
-            <th>
+            <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Today Deaths</h4>
             </th>
-            <th>
+            <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Top Active</h4>
             </th>
-            <th>
+            <th className="font-poppins text-[#2b2a69] text-lg font-bold text-left mb-[30px] pb-[10px] pl-[40px] mt-[30px] mb-[15px]">
               <h4>Top Recover</h4>
             </th>
           </tr>
@@ -54,6 +54,31 @@ const Tracker3 = () => {
         <tbody className="text-textBlue">
           {data.map((country) => (
             <tr key={country.country}>
+              <td className="bg-[#fffefe]">
+                <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
+                  <div className="self-center p-[10px]">
+                    {" "}
+                    {country.countryInfo && country.countryInfo.iso2 ? (
+                      <img
+                        src={`https://disease.sh/assets/img/flags/${country.countryInfo.iso2.toLowerCase()}.png`}
+                        className="w-[50px] h-[50px] object-cover rounded-full"
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+
+                  <div className="flex flex-col items-left pl-3">
+                    <div className="font-poppins font-medium text-xs text-[#2b2a69]">
+                      {country.country}
+                    </div>
+                    <div className="font-roboto font-bold text-lg text-[#2b2a69]">
+                      {country.cases}
+                    </div>
+                  </div>
+                </div>
+              </td>
+
               <td className="bg-[#fffefe]">
                 <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
                   <div className="self-center">
@@ -79,62 +104,102 @@ const Tracker3 = () => {
                 </div>
               </td>
 
-              <td>
-                <div className="flex flex-row">
-                  <div>
+              <td className="bg-[#fffefe]">
+                <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
+                  <div className="self-center">
                     {" "}
                     {country.countryInfo && country.countryInfo.iso2 ? (
                       <img
                         src={`https://disease.sh/assets/img/flags/${country.countryInfo.iso2.toLowerCase()}.png`}
-                        className="w-[30px]"
+                        className="w-[50px] h-[50px] object-cover rounded-full"
                       />
                     ) : (
                       ""
                     )}
                   </div>
-                  <div className="flex flex-col">
-                    <div>{country.country}</div>
-                    <div className="font-roboto">{country.todayCases}</div>
+
+                  <div className="flex flex-col items-left pl-3">
+                    <div className="font-poppins font-medium text-xs text-[#2b2a69]">
+                      {country.country}
+                    </div>
+                    <div className="font-roboto font-bold text-lg text-[#2b2a69]">
+                      {country.cases}
+                    </div>
                   </div>
                 </div>
               </td>
 
               <td className="bg-[#fffefe]">
-                <div className="flex flex-row">
-                  <div>
+                <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
+                  <div className="self-center">
                     {" "}
                     {country.countryInfo && country.countryInfo.iso2 ? (
                       <img
                         src={`https://disease.sh/assets/img/flags/${country.countryInfo.iso2.toLowerCase()}.png`}
-                        className="w-[30px]"
+                        className="w-[50px] h-[50px] object-cover rounded-full"
                       />
                     ) : (
                       ""
                     )}
                   </div>
-                  <div className="flex flex-col">
-                    <div>{country.country}</div>
-                    <div className="font-roboto">{country.cases}</div>
+
+                  <div className="flex flex-col items-left pl-3">
+                    <div className="font-poppins font-medium text-xs text-[#2b2a69]">
+                      {country.country}
+                    </div>
+                    <div className="font-roboto font-bold text-lg text-[#2b2a69]">
+                      {country.cases}
+                    </div>
                   </div>
                 </div>
               </td>
 
               <td className="bg-[#fffefe]">
-                <div className="flex flex-row">
-                  <div>
+                <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
+                  <div className="self-center">
                     {" "}
                     {country.countryInfo && country.countryInfo.iso2 ? (
                       <img
                         src={`https://disease.sh/assets/img/flags/${country.countryInfo.iso2.toLowerCase()}.png`}
-                        className="w-[30px]"
+                        className="w-[50px] h-[50px] object-cover rounded-full"
                       />
                     ) : (
                       ""
                     )}
                   </div>
-                  <div className="flex flex-col">
-                    <div>{country.country}</div>
-                    <div className="font-roboto">{country.cases}</div>
+
+                  <div className="flex flex-col items-left pl-3">
+                    <div className="font-poppins font-medium text-xs text-[#2b2a69]">
+                      {country.country}
+                    </div>
+                    <div className="font-roboto font-bold text-lg text-[#2b2a69]">
+                      {country.cases}
+                    </div>
+                  </div>
+                </div>
+              </td>
+
+              <td className="bg-[#fffefe]">
+                <div className="flex flex-row m-10 justify-around p-3 border-2 border-sky-500 w-200">
+                  <div className="self-center">
+                    {" "}
+                    {country.countryInfo && country.countryInfo.iso2 ? (
+                      <img
+                        src={`https://disease.sh/assets/img/flags/${country.countryInfo.iso2.toLowerCase()}.png`}
+                        className="w-[50px] h-[50px] object-cover rounded-full"
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+
+                  <div className="flex flex-col items-left pl-3">
+                    <div className="font-poppins font-medium text-xs text-[#2b2a69]">
+                      {country.country}
+                    </div>
+                    <div className="font-roboto font-bold text-lg text-[#2b2a69]">
+                      {country.cases}
+                    </div>
                   </div>
                 </div>
               </td>
