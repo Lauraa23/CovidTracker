2df5e11d10e431ad5dcc8f3feef26eedb3804342
Deiximar Virriel <deiximar@gmail.com>
2024-06-30 13:55:45 +0200

    Arreglar responsive despues de merge

--- src/components/tracker_zero/TrackerZero.jsx
+++ src/components/tracker_zero/TrackerZero.jsx
@@ -5,8 +5,8 @@ import TopCountries from "./TopCountries";
 const TrackerZero = () => {
   return (
     <div
-      className="w-90 px-3 py-5 border-r border-solid border-strongGray z-1 h-screen 
-      scrollBar overflow-hidden focus:overflow-y-scroll hover:overflow-y-scroll active:overflow-y-scroll scroll-smooth cursor-grab max-[1200px]:hidden"
+      className="w-full xl:min-w-[400px] lg:min-w-[200px] px-3 py-5 border-r border-solid border-strongGray z-1 h-screen 
+      scrollBar overflow-hidden focus:overflow-y-scroll hover:overflow-y-scroll active:overflow-y-scroll scroll-smooth cursor-grab hidden lg:block"
     >
       <GeneralInformation />
       <TopCountries />
